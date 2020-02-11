775bdd78fa87d7aaef8b5a6b97244d3d37b7701d
ShaunFerns <shaun.ferns@tudublin.ie>
2020-02-11 14:08:26 +0000

    1st commit

---
+++ Assets/_Scripts/DisplayChangedTextContent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayChangedTextContent : MonoBehaviour
+{
+    public InputField inputField;
+    private Text textDisplay;
+
+    void Awake()
+    {
+        textDisplay = GetComponent<Text>();
+    }
+
+    public void DisplayNewValue()
+    {
+        textDisplay.text = "last entry = '" + inputField.text + "'";
+    }
+}
